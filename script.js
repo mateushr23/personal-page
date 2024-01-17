5685e020ccf79fa8ec7467c7d2d33c9744d5dc4a
mateushr23 <mateushr23@gmail.com>
2024-01-17 12:39:22 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //pegar tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/img2.png")
+    img.setAttribute("alt", "light on")
+  } else {
+    img.setAttribute("src", "./assets/img1.png")
+    img.setAttribute("alt", "light off")
+  }
+}
